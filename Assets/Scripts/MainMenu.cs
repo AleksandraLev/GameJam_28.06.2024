4efddf0bf40abf3b65585b13a1e7689cea937674
Aleksandra <sasha.levashova@mail.ru>
2024-06-28 14:56:35 +0300

    Создан шаблон меню

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Выход из игры");
+        Application.Quit();
+    }
+
+}
